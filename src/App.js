04ad45d249964279bb570059805c25214ba067bc
alphakahere <alphakahere@gmail.com>
2022-01-24 01:24:04 +0000

    ecrire le schema de validation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,31 @@ import {Formik, Field, Form, ErrorMessage} from 'formik';
 import * as Yup from 'yup';
 import "bootstrap/dist/css/bootstrap.css";
 
+const validationSchema = Yup.object().shape({
+    firstName: Yup.string()
+        .min(5, "trop petit")
+        .max(50, "trop long!")
+        .required("Ce champ est obligatoire"),
+    lastName: Yup.string()
+        .min(2, "trop petit")
+        .max(10, "trop long!")
+        .required("Ce champ est obligatoire"),
+    email: Yup.string()
+        .email("email invalide")
+        .required("l'email est obligatoire"),
+    password: Yup.string()
+        .required("Mot de passe est obligatoire")
+        .min(8, "Mot de passe doit être plus grand que 8 caractères")
+        .max(50, "Mot de passe doit être plus petit que 50 caractères"),
+    confirmPassword: Yup.string()
+        .required("Confirmation de mot de passe est obligatoire")
+        .oneOf(
+            [Yup.ref("password"), null],
+            "Le mot de passe de confirmation ne correspond pas"
+        ),
+    acceptTerms: Yup.bool().oneOf([true], "Accepter les conditions est obligatoire"),
+});
+
 const App = () => {
 	return (
 		<div className="register-form container col-md-4 pt-5">
